25436f49bdcebb32b2e014afb9af713f6953e524
lrinkedewit <leonoor.rinkedewit@dazn.com>
2022-11-27 11:55:21 +0100

    refactor: removed object from global scope and turned it into a message to prevent scope pollution

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { vestedEquityCalculator } from "./vestedEquityCalculator.js";
 export default {
   async fetch(request) {
     let input = await request.json();
-    const output = vestedEquityCalculator(input);
+    const output = JSON.stringify(vestedEquityCalculator(input));
     console.log(output)
     return new Response(output)
   },
--- src/vestedEquityCalculator.js
+++ src/vestedEquityCalculator.js
@@ -1,66 +1,57 @@
-import { incrementMonth } from "./utils.js";
-// import { start_date } from "./variables.js";
-// import { cliff_months } from "./variables.js";
+import { incrementMonth } from "./utils.js";0
 
 // EXAMPLE INPUT
-const input = {
-  "option_grants": [
-    {
-      "quantity": 2400,
-      "start_date": "01-01-2018",
-      "cliff_months": 12,
-      "duration_months": 24
-    }
-  ],
-  "company_valuations": [
-    {
-      "price": 10,
-      "valuation_date": "09-12-2017"
-    },
-    {
-      "price": 12,
-      "valuation_date": "09-07-2018" 
-    },
-    {
-      "price": 15,
-      "valuation_date": "03-02-2019"
-    }
-  ]
-}
+// const input = {
+//   "option_grants": [sws zxcvg bhnjmk,l.ol.
+//     {
+//       "quantity": 2400,
+//       "start_date": "01-01-2018",
+//       "cliff_months": 12,
+//       "duration_months": 24
+//     }
+//   ],
+//   "company_valuations": [
+//     {
+//       "price": 10,
+//       "valuation_date": "09-12-2017"
+//     },
+//     {
+//       "price": 12,
+//       "valuation_date": "09-07-2018" 
+//     },
+//     {
+//       "price": 15,
+//       "valuation_date": "03-02-2019"
+//     }
+//   ]
+// }
 
 
-const extractedValues = {
+// const vestingMessage = {
 
-  // values below are initated by invoking extractStartDate()
-  extractedStartDate : "", // "01-01-2018",
+//   // values below are initated by invoking extractStartDate()
+//   extractedStartDate : "", // "01-01-2018",
 
-  // values below are initated by invoking extractPrices()
-  extractedPrices : [], // [10,12,15]
+//   // values below are initated by invoking extractPrices()
+//   extractedPrices : [], // [10,12,15]
 
-  // values below are initiated by invoking extractValuationDates()
-  valuationDatesInEffectDays : [], // [1,1,1]
-  valuationDatesInEffectMonths : [], // [1,8,3]
-  valuationDatesInEffectYears : [], // [2018,2018,2019]
+//   // values below are initiated by invoking extractValuationDates()
+//   valuationDatesInEffectDays : [], // [1,1,1]
+//   valuationDatesInEffectMonths : [], // [1,8,3]
+//   valuationDatesInEffectYears : [], // [2018,2018,2019]
 
-  // values below are created with durationValuationCountersArray function
-  durationValuationCounters : [] // [7,7,11]
-}
+//   // values below are created with durationValuationCountersArray function
+//   durationValuationCounters : [] // [7,7,11]
+// }
 
-
-// function that extracts the valuation dates and adds them to an array
-// throw an error if the date isn't in the right format
-const extractValuationDates = (req) => {
-  // declare array to keep track of days
+const extractValuationDates = (req, message) => {
   const days = [];
-  // declare array to keep track of months
   const months = [];
-  // declare array to keep track of years
   const years = [];
 
   // declare an array to hold all the valuation dates
   const valuationDates = [];
 
-  // iterate through array of objects and add them to an array
   for (let i = 0; i < req.company_valuations.length; i += 1) {
     valuationDates.push(req.company_valuations[i]["valuation_date"])
   }
@@ -93,22 +84,20 @@ const extractValuationDates = (req) => {
   for (let i = years.length - 1; i > 0; i -= 1) {
     let monthCounter = 0
     monthCounter = ((years[i] - years[i - 1]) * 12) + (months[i] - months[i - 1])
-    extractedValues.durationValuationCounters.push(monthCounter)
+    message.durationValuationCounters.push(monthCounter)
   }
 
   // adding final counter based on adding up durationValuationCounters and subtracting that from duration_months
   // add 1 to the value to see the price into the next month as ouput
-  let sumMonthCounters = extractedValues.durationValuationCounters.reduce((a,b) => a + b)
-  // console.log(`this is the sumMonths`, sumMonthCounters)
-  extractedValues.durationValuationCounters.push(req.option_grants[0].duration_months - sumMonthCounters + 1)
+  let sumMonthCounters = message.durationValuationCounters.reduce((a,b) => a + b)
+  message.durationValuationCounters.push(req.option_grants[0].duration_months - sumMonthCounters + 1)
 
-  extractedValues.valuationDatesInEffectDays = [...days]
-  extractedValues.valuationDatesInEffectMonths = [...months]
-  extractedValues.valuationDatesInEffectYears = [...years]
+  message.valuationDatesInEffectDays = [...days]
+  message.valuationDatesInEffectMonths = [...months]
+  message.valuationDatesInEffectYears = [...years]
 }
 
-// function that extracts the prices and places them in an array
-const extractPrices = (req) => {
+const extractPrices = (req, message) => {
 
   // array to hold the prices
   const prices = [];
@@ -118,18 +107,34 @@ const extractPrices = (req) => {
     prices.push(req.company_valuations[i]["price"])
   }
 
-  extractedValues.extractedPrices = [...prices]
+  message.extractedPrices = [...prices]
 }
 
-// function that extracts the startdate
-const extractStartDate = (req) => {
-  extractedValues.extractedStartDate += req.option_grants[0].start_date;
+const extractStartDate = (req, message) => {
+  message.extractedStartDate += req.option_grants[0].start_date;
 }
 
 export const vestedEquityCalculator = (req) => {
 
+  const vestingMessage = {
+
+    // values below are initated by invoking extractStartDate()
+    extractedStartDate : "", // "01-01-2018",
+  
+    // values below are initated by invoking extractPrices()
+    extractedPrices : [], // [10,12,15]
+  
+    // values below are initiated by invoking extractValuationDates()
+    valuationDatesInEffectDays : [], // [1,1,1]
+    valuationDatesInEffectMonths : [], // [1,8,3]
+    valuationDatesInEffectYears : [], // [2018,2018,2019]
+  
+    // values below are created with durationValuationCountersArray function
+    durationValuationCounters : [] // [7,7,11]
+  }
+
   let accumulatedPrice = 0;
-  let counter_cliff = req.cliff_months;
+  let counter_cliff = req.option_grants[0].cliff_months;
   const vestedEquityTimeline = [];
   const monthlyVestedEquityValue =
   {
@@ -137,15 +142,15 @@ export const vestedEquityCalculator = (req) => {
     "date": req.option_grants[0].start_date
   }
 
-  extractValuationDates(req)
-  extractPrices(req)
-  extractStartDate(req)
+  extractValuationDates(req, vestingMessage)
+  extractPrices(req, vestingMessage)
+  extractStartDate(req, vestingMessage)
 
   // for loop that lasts for the duration of the counter array
-  for (let i = 0; i < extractedValues.durationValuationCounters.length; i +=1) {
+  for (let i = 0; i < vestingMessage.durationValuationCounters.length; i +=1) {
     
-    // while counter_duration > -1, continue
-    while (extractedValues.durationValuationCounters[i] > 0) {
+    // while counter_duration > 0, continue
+    while (vestingMessage.durationValuationCounters[i] > 0) {
 
       // again, first check if the cliff has expired yet
       if (counter_cliff > 0) {
@@ -159,18 +164,17 @@ export const vestedEquityCalculator = (req) => {
       vestedEquityTimeline.push(monthlyVestedEquityValueShallowClone)
 
       // increment the accumulatedValue with the monthlyValueAdd
-      accumulatedPrice += extractedValues.extractedPrices[i];
+      accumulatedPrice += vestingMessage.extractedPrices[i];
       
       // increment the month by 1
       monthlyVestedEquityValue.date = incrementMonth(monthlyVestedEquityValue.date)
       
       // decrement counter_duration
-      extractedValues.durationValuationCounters[i] -= 1;
+      vestingMessage.durationValuationCounters[i] -= 1;
       counter_cliff -= 1;
     }
   }
 
-  
   return vestedEquityTimeline;
 }
 
